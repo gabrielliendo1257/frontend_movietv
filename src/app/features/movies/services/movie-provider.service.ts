7b78cdf73a089f3411cf17150c66c00f9f0d46bb
Gabriel L. <gabrielliendo1257@gmail.com>
2026-08-17 23:43:41 -0500

    feat(upload): MovieProviderService con endpoints de enrichment del BFF + create solo titulo
    - enrichmentSearch/enrichmentPreview/enrich contra /web/movies/enrichment/*
    - create() envia solo { title } (DRAFT)

--- src/app/features/movies/services/movie-provider.service.ts
+++ src/app/features/movies/services/movie-provider.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import { CreateMovieRequest, WebMovie } from '@features/movies/models/web-movie';
+import { WebMovie } from '@features/movies/models/web-movie';
+import { MovieDetails, MovieSummary, Pagination } from '@features/movies/models/the-movie-db';
+import { MovieMetadata } from '@features/uploads/models/movie-metadata';
 
 @Injectable({
     providedIn: 'root',
@@ -24,8 +26,26 @@ export class MovieProviderService {
         });
     }
 
-    create(request: CreateMovieRequest): Observable<WebMovie> {
-        return this.http.post<WebMovie>(this.baseUrl, request, {
+    create(title: string): Observable<WebMovie> {
+        return this.http.post<WebMovie>(this.baseUrl, { title }, { withCredentials: true });
+    }
+
+    enrichmentSearch(query: string): Observable<Pagination<MovieSummary>> {
+        return this.http.get<Pagination<MovieSummary>>(`${this.baseUrl}/enrichment/search`, {
+            params: { query },
+            withCredentials: true,
+        });
+    }
+
+    enrichmentPreview(tmdbId: number): Observable<MovieDetails> {
+        return this.http.get<MovieDetails>(`${this.baseUrl}/enrichment/preview`, {
+            params: { tmdb_id: String(tmdbId) },
+            withCredentials: true,
+        });
+    }
+
+    enrich(movieId: number, metadata: MovieMetadata): Observable<unknown> {
+        return this.http.post(`${this.baseUrl}/${movieId}/enrichment`, metadata, {
             withCredentials: true,
         });
     }
